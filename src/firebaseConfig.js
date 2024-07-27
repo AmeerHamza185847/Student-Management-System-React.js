2286536ea000396175f98a8dc5941cd957293725
AmeerHamza185847 <hamzaraza185847@gmail.com>
2024-07-27 02:21:32 -0700

    env updated

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,12 +4,12 @@ import {getFirestore} from 'firebase/firestore';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAL6oLbFgnu-uUS6O9bfSo1T2bQxRT1Mts",
-  authDomain: "student-management-syste-79669.firebaseapp.com",
-  projectId: "student-management-syste-79669",
-  storageBucket: "student-management-syste-79669.appspot.com",
-  messagingSenderId: "482797711748",
-  appId: "1:482797711748:web:d7fe701df1a0fb51406dd1"
+  apiKey: process.env.VITE_API_KEY,
+  authDomain:process.env.VITE_AUTH_DOMAIN,
+  projectId: process.env.VITE_PROJECT_ID,
+  storageBucket: process.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
